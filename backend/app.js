827a6334038908b22fbd6aea20e9c4cbd426764f
shahriarsajeeb <programmershahriarsajeeb@gmail.com>
2023-04-30 01:45:15 +0600

    frontend patch updated done

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://eshop-tutorial-cefl.vercel.app/",
     credentials: true,
   })
 );
