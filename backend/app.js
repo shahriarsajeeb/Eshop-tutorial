f397dcab7daf177c499cfe205301d769ed4dbb06
shahriarsajeeb <programmershahriarsajeeb@gmail.com>
2023-04-30 03:58:36 +0600

    cors remove done

--- backend/app.js
+++ backend/app.js
@@ -6,14 +6,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(cors({
-  origin: 'https://eshop-tutorial-cefl.vercel.app',
+  origin: '*',
   credentials: true
 }));
 
