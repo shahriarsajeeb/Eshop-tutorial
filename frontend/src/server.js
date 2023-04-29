5928df6b1fd54659bf5ec67397ef61cecd9e6d52
shahriarsajeeb <programmershahriarsajeeb@gmail.com>
2023-04-30 01:42:25 +0600

    backend url add done

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://eshop-tutorial.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000/"
+export const backend_url = "https://eshop-tutorial.vercel.app/"
