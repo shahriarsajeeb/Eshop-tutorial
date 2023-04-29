57b0bd76f90773d7bcfd3496abfc606a9c069a41
shahriarsajeeb <programmershahriarsajeeb@gmail.com>
2023-04-29 08:02:09 +0600

    done:

--- backend/app.js
+++ backend/app.js
@@ -14,12 +14,15 @@ app.use(
   })
 );
 app.use("/", express.static("uploads"));
+app.use("/", ((req,res) => {
+  res.send("Hello world!")
+}));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
-    path: "backend/config/.env",
+    path: "config/.env",
   });
 }
 
--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ process.on("uncaughtException", (err) => {
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
-    path: "backend/config/.env",
+    path: "config/.env",
   });
 }
 
