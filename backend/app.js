cdc8aeab72cc14c9537b97a5450439278e981a92
shahriarsajeeb <programmershahriarsajeeb@gmail.com>
2023-04-30 03:51:13 +0600

    cors fix done

--- backend/app.js
+++ backend/app.js
@@ -11,13 +11,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://eshop-tutorial-cefl-ngckxte6o-shahriarsajeeb.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+app.options("*", cors());
 
 app.use(express.json());
 app.use(cookieParser());
