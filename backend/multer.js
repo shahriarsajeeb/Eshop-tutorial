7e95dc0a59f7a042e0c0329499ac7636dbecb788
shahriarsajeeb <programmershahriarsajeeb@gmail.com>
2023-04-30 07:42:18 +0600

    multer error fix done

--- backend/multer.js
+++ backend/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination: function (req,res,cb){
-        cb(null, "uploads/");
+        cb(null, "./uploads");
     },
     filename: function (req,file,cb) {
         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
