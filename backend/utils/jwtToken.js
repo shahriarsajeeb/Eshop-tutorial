bf9e1f6e0d28cce7cb0e3dc7fa9f88bb58d0fefc
shahriarsajeeb <programmershahriarsajeeb@gmail.com>
2023-04-30 06:01:11 +0600

    cors fixed done

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,7 +7,7 @@ const sendToken = (user, statusCode, res) => {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     sameSite: "none",
-    Secure: true,
+    secure: true,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
