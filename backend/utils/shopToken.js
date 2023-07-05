bcef84d10c6843ccd511dd1882d6ec45cc2afa7c
talha3818 <tsrifattalha@gmail.com>
2023-07-05 19:10:30 +0600

    logout issue fix

--- backend/utils/shopToken.js
+++ backend/utils/shopToken.js
@@ -6,7 +6,6 @@ const sendShopToken = (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: "none",
     secure: true,
   };
 
